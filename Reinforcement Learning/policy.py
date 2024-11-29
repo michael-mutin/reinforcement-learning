e5371f0e677af9ee4d6868b880236e06575981ed
Michael Mutin <michi.mutin@gmail.com>
2024-11-29 11:15:10 +0100

    Add Q-Learning

--- Reinforcement Learning/policy.py
+++ Reinforcement Learning/policy.py
@@ -41,10 +41,10 @@ class Policy():
     def epsilon_greedy(self, epsilon, state: Tuple[int, int]) -> Action:
         return np.random.choice(self._possible_actions) if np.random.rand() < epsilon else Action(np.argmax(self._q_function[state]))
     
-    def sarsa(self, env: Environment, episodes=1000, learning_rate=0.3, discount=0.9, epsilon=0.3):
-        state = env.reset()
-
+    def sarsa(self, env: Environment, episodes=10000, learning_rate=0.3, discount=0.9, epsilon=0.3):
+        
         for i in range(episodes):
+            state = env.reset()
             print(f"Episode {i}")
             reached_terminal_state = False
             while not reached_terminal_state:
@@ -57,34 +57,42 @@ class Policy():
                 self._q_function[state+(action.value,)] = self._q_function[state+(action.value,)] + learning_rate * (reward + discount * self._q_function[next_state+(next_action.value,)] - self._q_function[state+(action.value,)])
                 
                 state = next_state
-                action = next_action
+                reached_terminal_state = done
+            env.reset()
+
+    def q_learning(self, env: Environment, episodes=10000, learning_rate=0.3, discount=0.9, epsilon=0.7):
+        
+        for i in range(episodes):
+            state = env.reset()
+            print(f"Episode {i}")
+            reached_terminal_state = False
+            while not reached_terminal_state:
+                action: Action = self.epsilon_greedy(epsilon, state)
+
+                next_state, reward, done = env.step(action)
+
+                self._q_function[state+(action.value,)] = self._q_function[state+(action.value,)] + learning_rate * (reward + discount * max([self._q_function[next_state + (possible_action.value,)] for possible_action in self._possible_actions]) - self._q_function[state+(action.value,)])
+                
+                state = next_state
                 reached_terminal_state = done
             env.reset()
 
     
-def vis_matrix(M: np.ndarray, cmap=plt.cm.Blues):
-    """Visualizes a matrix.
+    def vis_matrix(self, cmap=plt.cm.Blues):
+        """Visualizes a matrix.
 
-    :param M: The matrix to visualize.
-    :type M: np.ndarray
-    :param cmap: cmap., defaults to plt.cm.Blues
-    """
-    fig, ax = plt.subplots()
-    ax.matshow(M, cmap=cmap)
-    for i in range(M.shape[0]):
-        for j in range(M.shape[1]):
-            best_action = np.argmax(M[j, i])
-            c = M[j, i, best_action]
-            ax.text(i, j, "%.2f" % c, va="center", ha="center")
-    fig.show()
-
-def vis_q_function(M: np.ndarray):
-    for i in range(M.shape[0]):
-        for j in range(M.shape[1]):
-            best_action = np.argmax(M[i, j])
-            c = M[i, j, best_action]
-            print("%.2f " % c, end="")
-        print()
+        :param M: The matrix to visualize.
+        :type M: np.ndarray
+        :param cmap: cmap., defaults to plt.cm.Blues
+        """
+        greedy_arr = np.max(self._q_function, axis=-1)
+        fig, ax = plt.subplots()
+        ax.matshow(greedy_arr, cmap=cmap)
+        for i in range(greedy_arr.shape[0]):
+            for j in range(greedy_arr.shape[1]):
+                c = greedy_arr[j, i]
+                ax.text(i, j, "%.2f" % c, va="center", ha="center")
+        fig.show()
 
 
 import keyboard
@@ -100,8 +108,8 @@ env = Environment(n, goal_pos, (6,6))
 for hole_pos in holes_pos:
     env.create_hole(hole_pos)
 policy = Policy(n)
-policy.sarsa(env)
-vis_q_function(policy._q_function)
+policy.q_learning(env)
+policy.vis_matrix()
 input("Press ENTER to continue.")  
 initial_state = env.reset()
 env.print_grid()
